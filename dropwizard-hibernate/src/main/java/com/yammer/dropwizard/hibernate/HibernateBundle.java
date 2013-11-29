ddc66566c08d01be4832df6d3e731052982e3644
pnadeau <pnadeau@venasolutions.com>
2013-11-29 15:58:45 -0500

    Changed some private methods to protected removed final on others to make it easier to subclass this calss.  Added a sessionFactoryInitHook() to be called from run() when the SessionFactory is ready.  Addd some comments.

--- dropwizard-hibernate/src/main/java/com/yammer/dropwizard/hibernate/HibernateBundle.java
+++ dropwizard-hibernate/src/main/java/com/yammer/dropwizard/hibernate/HibernateBundle.java
@@ -11,10 +11,10 @@ import com.yammer.dropwizard.db.DatabaseConfiguration;
 import org.hibernate.SessionFactory;
 
 public abstract class HibernateBundle<T extends Configuration> implements ConfiguredBundle<T>, ConfigurationStrategy<T> {
-    private SessionFactory sessionFactory;
+    protected SessionFactory sessionFactory;
 
-    private final ImmutableList<Class<?>> entities;
-    private final SessionFactoryFactory sessionFactoryFactory;
+    protected final ImmutableList<Class<?>> entities;
+    protected final SessionFactoryFactory sessionFactoryFactory;
 
     protected HibernateBundle(Class<?> entity, Class<?>... entities) {
         this(ImmutableList.<Class<?>>builder().add(entity).add(entities).build(),
@@ -28,24 +28,48 @@ public abstract class HibernateBundle<T extends Configuration> implements Config
     }
 
     @Override
-    public final void initialize(Bootstrap<?> bootstrap) {
+    public void initialize(Bootstrap<?> bootstrap) {
         bootstrap.getObjectMapperFactory().registerModule(new Hibernate4Module());
     }
 
     @Override
-    public final void run(T configuration, Environment environment) throws Exception {
+    public void run(T configuration, Environment environment) throws Exception {
         final DatabaseConfiguration dbConfig = getDatabaseConfiguration(configuration);
         this.sessionFactory = sessionFactoryFactory.build(this, environment, dbConfig, entities);
         environment.addProvider(new UnitOfWorkResourceMethodDispatchAdapter(sessionFactory));
         environment.addHealthCheck(new SessionFactoryHealthCheck("hibernate",
                                                                  sessionFactory,
                                                                  dbConfig.getValidationQuery()));
+        
+        sessionFactoryInitHook(this.sessionFactory);
     }
 
-    public SessionFactory getSessionFactory() {
+   
+
+	public SessionFactory getSessionFactory() {
         return sessionFactory;
     }
 
+    /**
+     * Hook allowing for custom Hibernate SessionFactory customization.
+     * 
+     * Customize the configuration object to add filters or change naming strategies.
+     * 
+     * @param configuration
+     */
     public void configure(org.hibernate.cfg.Configuration configuration) {
     }
+    
+    /**
+     * Hook that will be called once from run() once the SessionFactory is created.
+     * 
+     * Useful for when you need access to the SessionFactory in another Bundle's
+     * intialization code.
+     * 
+     * 
+     * @param sessionFactory2
+     */
+    protected void sessionFactoryInitHook(SessionFactory sessionFactory2) {
+		
+	}
 }
